0813280e8f49101f2cb674a2385c763b9d5ad133
kavinprasanth <barryallean7274@gmail.com>
2024-06-19 07:21:22 -0700

    latest commit

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get('/get-news', async (req, res) => {
 app.post('/get-category', async (req, res) => {
     const apiKey = process.env.REACT_APP_GNEWS_API_KEY;
     var url = `https://newsapi.org/v2/everything?q=${req.data}&apiKey=e654a4c31be24f649f9da1577ae9d25d`;
+    console.log(url)
     const response = await fetch(url);
     const data = await response.json();
     res.send(data);
